ca24b820417bb830d28a0fa410b3599668ad9da0
JoynulIslam <joynul8763@gmail.com>
2026-04-10 20:27:27 +0600

    implement tourCategories

--- src/Components/index/Index.jsx
+++ src/Components/index/Index.jsx
@@ -2,6 +2,7 @@ import About from "./About/About";
 import BookingSteps from "./BookingSteps/BookingSteps";
 import Hero from "./Hero/Hero";
 import TopDestination from "./TopDestination/TopDestination";
+import TourCategories from "./TourCategories/TourCategories";
 import TourGuide from "./TourGuide/TourGuide";
 
 const Index = () => {
@@ -12,6 +13,7 @@ const Index = () => {
       <BookingSteps />
       <TopDestination />
       <TourGuide />
+      <TourCategories />
     </>
   );
 };

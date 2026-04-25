a7f826c2e0cfa634613b4e5976fdf00a52aebae6
JoynulIslam <joynul8763@gmail.com>
2026-04-25 13:44:13 +0600

    implement services section for About Page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -18,6 +18,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 import destination from "../../Data/DestinationCtg.json";
+import services from "../../Data/Services.json";
 
 import { Link } from "react-router-dom";
 import sectionBanner from "../../assets/section-banner.jpg";
@@ -25,6 +26,7 @@ import { useState } from "react";
 import { useRef } from "react";
 import DestinationCtgCard from "../../Components/DestinationCtgCard/DestinationCtgCard";
 import { Icon } from "@iconify/react";
+import Testimonials from "../../Components/index/Testimonials/Testimonials";
 
 export const About = () => {
   const [swiperInstance, setSwiperInstance] = useState(null);
@@ -210,6 +212,55 @@ export const About = () => {
           <img src={ballonRifht} alt="ballon-image" />
         </div>
       </div>
+
+      <div className="services bg-secondary px-[2%] sm:px-[8%] lg:px-[12%] md:py-[10%] relative">
+        <div className="title flex flex-col justify-center items-center text-center relative pb-10">
+          <h1 className="text-white text-4xl md:text-6xl font-bold">
+            <span className="text-yellow">Our Amazing </span>Services
+          </h1>
+          <p className="text-gray-200 my-2 text-lg">
+            Destinations worth exploring! Here are a few popular spots
+          </p>
+          <img
+            src={titleShape}
+            alt="titleShape"
+            className="w-[35%] object-contain absolute -bottom-12 brightness-0"
+          />
+        </div>
+
+        <div className="bg-[#daeeef] p-5 md:p-10 rounded-2xl my-10 md:my-14">
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+            {services.map((srv, idx) => {
+              return (
+                <>
+                  <Link
+                    to={`/services/${srv.id}`}
+                    className="services-item bg-white shadow-xl rounded-3xl p-3 group hover:bg-secondary transition-colors duration-500 cursor-pointer"
+                  >
+                    <div className="services-image rounded-3xl overflow-hidden h-60 md:h-100">
+                      <img
+                        src={srv.image}
+                        alt={srv.name}
+                        className="w-full h-full! object-cover"
+                      />
+                    </div>
+                    <span className="text-center block pt-5 py-2 text-2xl font-medium text-secondary group-hover:text-yellow transition-colors duration-500">
+                      {srv.name}
+                    </span>
+                  </Link>
+                </>
+              );
+            })}
+          </div>
+          <div className="absolute right-40 bottom-8 hidden lg:flex flex-col text-white font-kaushan! text-2xl text-end xl:text-5xl z-1">
+            Wornderful Services For You
+            <h2 className="uppercase font-afacad! font-extrabold text-4xl lg:text-6xl xl:text-8xl text-yellow">
+              Services We Offer
+            </h2>
+          </div>
+        </div>
+      </div>
+      <Testimonials />
     </>
   );
 };
